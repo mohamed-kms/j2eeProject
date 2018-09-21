cd10f696ea18f921cddfa6650a12a2776f01e952
Askia Mohamed <askiamk@gmail.com>
2018-09-21 15:47:08 +0200

    Gestion de compte bancaire avec une methode pour lister les compte et une autre pour les créer

---
+++ MBDS_Boukou_Kadri_Projet_J2EE/MBDS_Boukou_Kadri_Projet_J2EE-ejb/src/java/session/GestionDeCompteBancaire.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package session;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author mohamed-kms
+ */
+@Stateless
+@LocalBean
+public class GestionDeCompteBancaire {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
